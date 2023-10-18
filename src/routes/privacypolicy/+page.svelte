65479b45d8501b601187ca45e979aa72036f4307
ishan dutta <ishandutta2007@gmail.com>
2023-10-18 06:06:14 +0530

    HEroless

--- src/routes/privacypolicy/+page.svelte
+++ src/routes/privacypolicy/+page.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	import DesktopFeatures from '$lib/components/DesktopFeatures.svelte';
 	import DownloadSection from '$lib/components/DownloadSection.svelte';
-	import HeroSection from '$lib/components/HeroSection.svelte';
 	import SingularityLabsPrivacypolicSection from '$lib/components/SingularityLabsPrivacypolicSection.svelte';
 </script>
 
 <img src="/background_gradient.png" class="absolute h-full w-full" alt="background gradient" />
 
-<HeroSection />
 <SingularityLabsPrivacypolicSection />
 <DesktopFeatures />
 <DownloadSection />
