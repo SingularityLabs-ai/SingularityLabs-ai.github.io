c7a5786f559b84a12c7412e47cb69c8435a9611c
ishan dutta <ishandutta2007@gmail.com>
2023-10-18 07:32:02 +0530

    more to privacy policy contents

--- src/lib/components/SingularityLabsPrivacypolicSection.svelte
+++ src/lib/components/SingularityLabsPrivacypolicSection.svelte
@@ -182,12 +182,9 @@
         </h3>
 
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             We use Cookies and similar tracking technologies to track the activity on Our Service and store certain information. Tracking technologies used are beacons, tags, and scripts to collect and track information and to improve and analyze Our Service. The technologies We use may include:
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
-
             <li class="list-disc">
                 <b> Cookies or Browser Cookies.</b> A cookie is a small file placed on Your Device. You can instruct Your browser to refuse all Cookies or to indicate when a Cookie is being sent. However, if You do not accept Cookies, You may not be able to use some parts of our Service. Unless you have adjusted Your browser setting so that it will refuse Cookies, our Service may use Cookies.
 
@@ -197,12 +194,13 @@
             </li>
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies remain on Your personal computer or mobile device when You go offline, while Session Cookies are deleted as soon as You close Your web browser.
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
 
             We use both Session and Persistent Cookies for the purposes set out below:
+        </p>
+        <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
 
             <li class="list-disc">
                 <b>Necessary / Essential Cookies</b>
@@ -234,10 +232,8 @@
 
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             For more information about the cookies we use and your choices regarding cookies, please visit our Cookies Policy or the Cookies section of our Privacy Policy.
         </p>
-        <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
 
         <h1 class="text-left text-3xl font-semibold text-slate-200 sm:text-5xl">
             Use of Your Personal Data
@@ -248,39 +244,26 @@
         </h5>
 
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             <li class="list-disc">
                 <b>To provide and maintain our Service</b>, including to monitor the usage of our Service.
-
             </li>
             <li class="list-disc">
                 <b>To manage Your Account:</b> to manage Your registration as a user of the Service. The Personal Data You provide can give You access to different functionalities of the Service that are available to You as a registered user.
-
             </li>
             <li class="list-disc">
                 <b>For the performance of a contract:</b> the development, compliance and undertaking of the purchase contract for the products, items or services You have purchased or of any other contract with Us through the Service.
-
-
             </li>
             <li class="list-disc">
                 <b>To contact You:</b> To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application's push notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when necessary or reasonable for their implementation.
-
-
             </li>
             <li class="list-disc">
                 <b>To provide You</b> with news, special offers and general information about other goods, services and events which we offer that are similar to those that you have already purchased or enquired about unless You have opted not to receive such information.
-
-
             </li>
             <li class="list-disc">
                 <b>To manage Your requests:</b> To attend and manage Your requests to Us.
-
-
             </li>
             <li class="list-disc">
                 <b>For business transfers:</b> We may use Your information to evaluate or conduct a merger, divestiture, restructuring, reorganization, dissolution, or other sale or transfer of some or all of Our assets, whether as a going concern or as part of bankruptcy, liquidation, or similar proceeding, in which Personal Data held by Us about our Service users is among the assets transferred.
-
-
             </li>
             <li class="list-disc">
                 <b>For other purposes</b>: We may use Your information for other purposes, such as data analysis, identifying usage trends, determining the effectiveness of our promotional campaigns and to evaluate and improve our Service, products, services, marketing and your experience.
@@ -292,32 +275,23 @@
         </h5>
 
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
-
             <li class="list-disc">
                 <b>With Service Providers:</b> We may share Your personal information with Service Providers to monitor and analyze the use of our Service, to contact You.
-
             </li>
             <li class="list-disc">
-
                 <b>For business transfers:</b> We may share or transfer Your personal information in connection with, or during negotiations of, any merger, sale of Company assets, financing, or acquisition of all or a portion of Our business to another company.
-
             </li>
             <li class="list-disc">
                 <b>With Affiliates:</b> We may share Your information with Our affiliates, in which case we will require those affiliates to honor this Privacy Policy. Affiliates include Our parent company and any other subsidiaries, joint venture partners or other companies that We control or that are under common control with Us.
-
             </li>
             <li class="list-disc">
                 <b>With business partners:</b> We may share Your information with Our business partners to offer You certain products, services or promotions.
-
             </li>
             <li class="list-disc">
                 <b>With other users:</b> when You share personal information or otherwise interact in the public areas with other users, such information may be viewed by all users and may be publicly distributed outside. If You interact with other users or register through a Third-Party Social Media Service, Your contacts on the Third-Party Social Media Service may see Your name, profile, pictures and description of Your activity. Similarly, other users will be able to view descriptions of Your activity, communicate with You and view Your profile.
-
             </li>
             <li class="list-disc">
                 <b>With Your consent</b>: We may disclose Your personal information for any other purpose with Your consent.
-
             </li>
         </p>
 
@@ -326,11 +300,9 @@
         </h1>
 
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             The Company will retain Your Personal Data only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use Your Personal Data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             The Company will also retain Usage Data for internal analysis purposes. Usage Data is generally retained for a shorter period of time, except when this data is used to strengthen the security or to improve the functionality of Our Service, or We are legally obligated to retain this data for longer time periods.
         </p>
 
@@ -339,43 +311,31 @@
         </h1>
 
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             Your consent to this Privacy Policy followed by Your submission of such information represents Your agreement to that transfer.
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             The Company will take all steps reasonably necessary to ensure that Your data is treated securely and in accordance with this Privacy Policy and no transfer of Your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of Your data and other personal information.
         </p>
-        <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
 
         <h1 class="text-left text-3xl font-semibold text-slate-200 sm:text-5xl">
             Delete Your Personal Data
         </h1>
 
-
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
             You have the right to delete or request that We assist in deleting the Personal Data that We have collected about You.
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             Our Service may give You the ability to delete certain information about You from within the Service.
-
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             You may update, amend, or delete Your information at any time by signing in to Your Account, if you have one, and visiting the account settings section that allows you to manage Your personal information. You may also contact Us to request access to, correct, or delete any personal information that You have provided to Us.
-
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             Please note, however, that We may need to retain certain information when we have a legal obligation or lawful basis to do so.
         </p>
-
 
         <h1 class="text-left text-3xl font-semibold text-slate-200 sm:text-5xl">
             Disclosure of Your Personal Data
@@ -396,7 +356,6 @@
             Under certain circumstances, the Company may be required to disclose Your Personal Data if required to do so by law or in response to valid requests by public authorities (e.g. a court or a government agency).
         </p>
 
-
         <h3 class="text-left text-xl font-semibold text-slate-200 sm:text-3xl">
             Other legal requirements
         </h3>
@@ -405,27 +364,22 @@
         The Company may disclose Your Personal Data in the good faith belief that such action is necessary to:
         </h5>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             <li class="list-disc">
                 Comply with a legal obligation
-
             </li>
             <li class="list-disc">
                 Protect and defend the rights or property of the Company
-
             </li>
             <li class="list-disc">
                 Prevent or investigate possible wrongdoing in connection with the Service
-
             </li>
             <li class="list-disc">
                 Protect the personal safety of Users of the Service or the public
-
             </li>
             <li class="list-disc">
                 Protect against legal liability
-
             </li>
+        </p>
 
         <h1 class="text-left text-3xl font-semibold text-slate-200 sm:text-5xl">
             Security of Your Personal Data
@@ -443,10 +397,8 @@
             Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.
         </p>
-
 
         <h1 class="text-left text-3xl font-semibold text-slate-200 sm:text-5xl">
             Links to Other Websites
@@ -456,12 +408,8 @@
             Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.
-
         </p>
-        <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
 
         <h1 class="text-left text-3xl font-semibold text-slate-200 sm:text-5xl">
             Changes to this Privacy Policy
@@ -469,16 +417,13 @@
 
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
             We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy Policy on this page.
-
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
             We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the "Last updated" date at the top of this Privacy Policy.
-
         </p>
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
             You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.
         </p>
-
 
         <h1 class="text-left text-3xl font-semibold text-slate-200 sm:text-5xl">
             Contact Us
@@ -489,7 +434,6 @@
         </h5>
 
         <p class="text-left text-slate-400 sm:max-w-2xl sm:text-xl">
-
             <li class="list-disc">
                 By email: labssingularity@gmail.com
             </li>
