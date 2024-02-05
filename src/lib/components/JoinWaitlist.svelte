08c8f83cfe5a76474fa64c5beebf1de8374f08d8
ishan dutta <ishandutta2007@gmail.com>
2024-02-05 06:27:08 +0530

    script.google.com_alert

--- src/lib/components/JoinWaitlist.svelte
+++ src/lib/components/JoinWaitlist.svelte
@@ -27,7 +27,12 @@
         <br />
         Email:<br />
         <input name="Email" type="email">
-        <button type="submit">Subscribe!</button>
+        <button type="submit"
+            on:click={async () => {
+                alert(
+                    'Thank you for joining the waitlist! We will notify you when SingularityLabs Cloud is ready for you.'
+                );
+            }}>Subscribe!</button>
     </form>
 
 	<div class="flex flex-col gap-5">
