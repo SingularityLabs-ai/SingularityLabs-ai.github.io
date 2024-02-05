135c9969a62c7e470a9939988e6cc3fe11de0ab9
ishan dutta <ishandutta2007@gmail.com>
2024-02-05 07:01:36 +0530

    script.google.com_alert

--- src/lib/components/JoinWaitlist.svelte
+++ src/lib/components/JoinWaitlist.svelte
@@ -18,16 +18,14 @@
 		Join the beta waitlist for <u class="text-slate-100">free</u>
 	</h3>
 
-    <form
-        method="POST" 
-        action="https://script.google.com/macros/s/AKfycbyhhb7ueo1PTnhUqTEMXBITXSPji7V8soD21LXm9IYZXzb_0Ffm30wUlu8eh1fuYuSi/exec"
-        id="myform">
-        Name:<br />
-        <input name="Name" type="text" bind:value={name} >
-        <br />
-        Email:<br />
-        <input name="Email" type="email" bind:value={email}>
-        <button type="submit" on:click={async () => {
+    <form method="POST" action="https://script.google.com/macros/s/AKfycbyhhb7ueo1PTnhUqTEMXBITXSPji7V8soD21LXm9IYZXzb_0Ffm30wUlu8eh1fuYuSi/exec" id="myform">
+        <div class="flex flex-col gap-4">
+            <input placeholder="Your Name" name="Name" type="text" bind:value={name} >
+            <br />
+            <input placeholder="Your Email" name="Email" type="email" bind:value={email}>
+        </div>
+        <button 
+            class="ring-ring-500 rounded-full bg-green-600 px-8 py-3 font-medium text-green-200 ring-1 ring-green-500 transition-all duration-500 hover:bg-green-500" type="submit" on:click={async () => {
             if (name && email && name.length < 256 && emailRegex.test(email)) {
                 alert('Thank you for joining the waitlist! We will notify you when SingularityLabs Cloud is ready for you.');
             } else {
